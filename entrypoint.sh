ac55402056785850a7af2dd3111800f72eaf4806
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2021-03-24 00:32:19 +0100

    fix: Remove CRLF from .adr-dir (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
+if [ -f .adr-dir ]; then
+  dos2unix .adr-dir
+fi
+
 adr "$@"
 
-if [ -f README.md ]; then
+if [ -f README.md ] && [ -f .adr-dir ]; then
   adr-log -i README.md -d "$(cat .adr-dir)"
 fi
