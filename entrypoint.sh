6caefa4f5a22669d0b88f721be5c733e1cf054b2
Håkon Botnmark Jahre <hakon.botnmark.jahre@extendaretail.com>
2021-10-05 09:42:33 +0200

    fix: Update ADR list in all Markdown files with a line matching `<!-- adrlog` (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 set -e
 
 if [ -f .adr-dir ]; then
@@ -8,8 +9,11 @@ fi
 adr "$@"
 
 if [ -f .adr-dir ]; then
-  targetFile="$(grep -m 1 -l ./*.md -e '<\!-- adrlog')"
-  if [ -n "$targetFile" ]; then
-    adr-log -i "$targetFile" -d "$(cat .adr-dir)"
-  fi
+  files="$(grep -m 1 -l ./*.md -e '<\!-- adrlog')"
+
+  for targetFile in ${files}; do
+    if [ -n "${targetFile}" ]; then
+      adr-log -i "${targetFile}" -d "$(cat .adr-dir)"
+    fi
+  done
 fi
